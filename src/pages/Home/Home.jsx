b77502f643b41e2eb5f212aa52bab28ad5637f6d
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-03-02 16:57:38 +0530

    Adding the aos animations package

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Bs123 } from "react-icons/bs";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(()=>{
+    AOS.init({
+      duration: 2000,
+    });
+  },[])
   return (
     <div>
-      <p className='underline'>Home <span className='text-fuchsia-800 text-4xl'><Bs123 /></span> </p>
+      <p className="underline">
+        Home
+        <span className="text-fuchsia-800 text-4xl">
+          <Bs123 />
+        </span>
+      </p>
+      <div className="box bg-red-500 w-7" data-aos="fade-up">s</div>
+      <div className="box bg-blue-200 w-7"  data-aos="zoom-in-down">g</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
